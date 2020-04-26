2a1bf5f500aac9a87ac6224869de7dd694f08b5a
Karol Kolanski <gralu86@gmail.com>
2020-04-26 13:56:59 +0200

    Zamykanie alerta

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import unittest
 from selenium import webdriver
+from time import sleep # sleep(2)
+# import time ->  time.sleep(2)
 
 class MojPrzypadekTestowy(unittest.TestCase):
     def setUp(self):
@@ -10,9 +12,14 @@ class MojPrzypadekTestowy(unittest.TestCase):
 
     def testSelenium(self):
         driver = self.driver
+        print("Wylaczam alert")
+        al = driver.find_element_by_id('onesignal-popover-cancel-button')
+        al.click()
         print("Klikam zaloguj sie")
         zaloguj_btn = driver.find_element_by_xpath('//span[text()=" zaloguj się"]')
         zaloguj_btn.click()
+        # Sleep tylko w celach debagowych
+        sleep(2)
 
     def tearDown(self):
         print("sprzatanie po tescie")
