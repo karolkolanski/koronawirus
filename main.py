db635f5a48dfc972add6cc6598623f3a2a48faf6
Karol Kolanski <gralu86@gmail.com>
2020-04-26 13:41:47 +0200

    Wersja pierwsza

--- main.py
+++ main.py
@@ -5,11 +5,19 @@ class MojPrzypadekTestowy(unittest.TestCase):
     def setUp(self):
         print("Przygotowanie testu")
         self.driver = webdriver.Firefox()
+        self.driver.get("https://www.sukienkimm.pl/")
+        self.driver.maximize_window()
 
     def testSelenium(self):
-        print("Test wlasciwy")
+        driver = self.driver
+        print("Klikam zaloguj sie")
+        zaloguj_btn = driver.find_element_by_xpath('//span[text()=" zaloguj się"]')
+        zaloguj_btn.click()
+
     def tearDown(self):
         print("sprzatanie po tescie")
+        self.driver.quit()
+
 
 
 
